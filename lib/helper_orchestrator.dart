111ea00ea32f52c11d3b27500b4795ebc53e3edb
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 20:33:46 +0200

    /// Hide the current overlayed helper
     /// Hide the current overlayed helper

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -151,4 +151,5 @@ class HelperOrchestratorState extends State<HelperOrchestrator> {
   @override
   Widget build(BuildContext context) {
     return _HelperOrchestratorScope(
+      helperOrchestratorState: this,
   
