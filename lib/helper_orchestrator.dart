a59f81d9cb7e0303c6e6093a36053171d61b551f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 20:20:56 +0200

    Update helper_orchestrator.dart

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -6,4 +6,4 @@ import 'anchored/anchored_helper_widget.dart';
 import 'services/overlay_helper.dart';
 
 class _HelperOrchestratorScope extends InheritedWidget {
-  final HelperOrchestratorState _helperOrchestratorSta
+  final HelperOrchestratorState _helperOrchestratorState
