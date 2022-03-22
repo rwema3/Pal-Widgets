a7880d2059e453aacd290424767cc2e874aa89d5
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 20:23:46 +0200

    bool updateShouldNotify(_HelperOrchestratorScope old) =>
    /// Manages Pal widgets to display an [Overlay] over your pages
    /// this will allow you to register widgets and show [AnchoredHelper] and others
    /// Pal onboarding widgets

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -40,3 +40,4 @@ class HelperOrchestrator extends StatefulWidget {
     final _HelperOrchestratorScope scope =
         context.dependOnInheritedWidgetOfExactType<_HelperOrchestratorScope>()!;
     return scope._helperOrchestratorState;
+  }
