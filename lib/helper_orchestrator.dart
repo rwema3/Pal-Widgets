25c1ed06ffa87887e6b515729b62dfa933109cc6
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 21:16:36 +0200

    Future showAnchoredHelper(String anchorKeyId, AnchoredHelper helper) async {     final anchor = await findAnchor(anchorKeyId);     if (anchor == null) {       debugPrint("anchor cannot be found. show anchored failed");       return;     }
     Future showAnchoredHelper(String anchorKeyId, AnchoredHelper helper) async {
        final anchor = await findAnchor(anchorKeyId);
        if (anchor == null) {
          debugPrint("anchor cannot be found. show anchored failed");
          return;
        }

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -135,3 +135,4 @@ class HelperOrchestratorState extends State<HelperOrchestrator> {
     final anchorSize = element.bounds!.size;
     final currentPos = element.offset!;
     final writeArea = _elementFinder!.getLargestAvailableSpace(element);
+    return Anchor(
