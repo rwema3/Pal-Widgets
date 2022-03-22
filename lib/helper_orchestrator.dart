060526f9f5d361dbca1a02632586aefd496ca824
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 19:53:32 +0200

    Update helper_orchestrator.dart

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -12,5 +12,4 @@ class _HelperOrchestratorScope extends InheritedWidget {
     Key? key,
     required Widget child,
     required HelperOrchestratorState helperOrchestratorState,
-  })  : _helperOrchestratorState = helperOrchestratorState,
-        
+  })  : 
