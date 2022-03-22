3e19b68b73b3074c26618be3261a3e9e8283b21e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 21:18:20 +0200

    Future showAnchoredHelper(String anchorKeyId, AnchoredHelper helper) async {     final anchor = await findAnchor(anchorKeyId);     if (anchor == null) {       debugPrint("anchor cannot be found. show anchored failed");       return;     }

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -153,4 +153,4 @@ class HelperOrchestratorState extends State<HelperOrchestrator> {
     return _HelperOrchestratorScope(
       helperOrchestratorState: this,
       child: widget.child,
-    );
+    );  }
