73b8a4dd267b7071c5bb56519ff38b509e8737c2
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 21:10:13 +0200

    Update helper_orchestrator.dart

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -35,3 +35,4 @@ class HelperOrchestrator extends StatefulWidget {
   ///  * [maybeOf], which is a similar function but will return null instead of
   ///    throwing if there is no [HelperOrchestrator] ancestor.
   static HelperOrchestratorState of(BuildContext context) {
+    assert(debugCheckHasScaffoldMessenger(context));
