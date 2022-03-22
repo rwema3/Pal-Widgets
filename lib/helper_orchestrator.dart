571411844a092b24b37f060c0ec8b3f4b6f86fd4
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 20:45:15 +0200

    Update helper_orchestrator.dart

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -142,7 +142,7 @@ class HelperOrchestratorState extends State<HelperOrchestrator> {
     );
   }
 
-  /// Hide the current 
+  /// Hide the current
   void hideHelper() {
     _overlayHelper.popHelper();
   }
