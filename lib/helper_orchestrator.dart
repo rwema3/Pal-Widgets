ebf46f50ceb280fba11391f3e99568a8816f71d4
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 20:03:33 +0200

    Update helper_orchestrator.dart

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -4,6 +4,3 @@ import 'package:pal_widgets/services/element_finder.dart';
 import 'anchored/anchor_model.dart';
 import 'anchored/anchored_helper_widget.dart';
 import 'services/overlay_helper.dart';
-
-class _HelperOrchestratorScope extends InheritedWidget {
-  
