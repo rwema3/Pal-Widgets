b5534305e3f584547b1c1deae54e7ea995090978
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 20:23:27 +0200

    Update helper_orchestrator.dart

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -39,3 +39,4 @@ class HelperOrchestrator extends StatefulWidget {
 
     final _HelperOrchestratorScope scope =
         context.dependOnInheritedWidgetOfExactType<_HelperOrchestratorScope>()!;
+    return scope._helperOrchestratorState;
