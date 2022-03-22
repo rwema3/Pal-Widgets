730961cf646fefddf914306d7133af90622fe370
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 20:05:45 +0200

    Update helper_orchestrator.dart

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:pal_widgets/services/element_finder.dart';
 
 import 'anchored/anchor_model.dart';
-import 'anchored/anchored_helper_widge
+import 'anchored/anchored_helper_widg
