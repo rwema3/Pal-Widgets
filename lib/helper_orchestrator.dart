ce7ed72d9babb362275937dacd071be482b05edb
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 19:52:14 +0200

    Update helper_orchestrator.dart

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -28,4 +28,5 @@ class HelperOrchestrator extends StatefulWidget {
 
   const HelperOrchestrator({Key? key, required this.child}) : super(key: key);
 
-  /// If there is no [HelperOrchestrator] in scope  ///
+  /// If there is no [HelperOrchestrator] in scope
+  ///
