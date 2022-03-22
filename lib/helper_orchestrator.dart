ab6bad06f357688cbd1dfecd90fe259d358ccb58
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 21:05:11 +0200

    Update helper_orchestrator.dart

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -52,4 +52,3 @@ class HelperOrchestrator extends StatefulWidget {
   ///  * [of], which is a similar function, except that it will throw an
   ///    exception if a [HelperOrchestrator] is not found in the given context.
   static HelperOrchestratorState? maybeOf(BuildContext? context) {
-    if (context == null) {
