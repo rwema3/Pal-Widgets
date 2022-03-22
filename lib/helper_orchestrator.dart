6edc89593516c6d8e8b0fc056107a4207b608c19
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-22 21:12:02 +0200

    Update helper_orchestrator.dart

--- lib/helper_orchestrator.dart
+++ lib/helper_orchestrator.dart
@@ -71,3 +71,5 @@ class HelperOrchestrator extends StatefulWidget {
 class HelperOrchestratorState extends State<HelperOrchestrator> {
   final Map<String, Key> keys = {};
   final OverlayHelper _overlayHelper = OverlayHelper();
+  ElementFinder? _elementFinder;
+
